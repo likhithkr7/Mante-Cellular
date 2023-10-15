0055d43809cf972579b15b0ae641971223001948
Likhith.R.Krishna <likhithkr72gmail.com>
2023-10-15 13:24:53 +0530

    added content to center

--- src/components/SubSection.jsx
+++ src/components/SubSection.jsx
@@ -1,24 +1,31 @@
-import { Container, Row } from "react-bootstrap"
-import SubProduct from "./Product/SubProduct"
+import { Container, Row } from "react-bootstrap";
+import SubProduct from "./Product/SubProduct";
 
-
-const SubSection = ({ title, bgColor , productItems, addToCart }) => {
-return (
-    <section style={{ background:bgColor }}> 
-        <Container>
-            <div className='heading'>
-                <h1>{title}</h1>
-            </div>
-            <Row className="justify-content-center">
-                {productItems.map((productItem) => {
-                return (
-                    <SubProduct key={productItem.id} title={title} productItem={productItem} addToCart={addToCart}/>
-                )
-                })}
-            </Row>
-        </Container>
+const SubSection = ({ title, bgColor, productItems, addToCart }) => {
+  return (
+    <section style={{ background: bgColor }}>
+      <Container>
+        <div className="heading">
+          <h1>{title}</h1>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <h6>To avail best offers please contact Ph: +91 9148114488</h6>
+        </div>
+        <Row className="justify-content-center">
+          {productItems.map((productItem) => {
+            return (
+              <SubProduct
+                key={productItem.id}
+                title={title}
+                productItem={productItem}
+                addToCart={addToCart}
+              />
+            );
+          })}
+        </Row>
+      </Container>
     </section>
-    )
-}
+  );
+};
 
-export default SubSection
+export default SubSection;
