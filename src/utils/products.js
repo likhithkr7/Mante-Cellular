5c5f8cc6ddde769df4daac14c661f8b37c8a8827
Likhith Krishna <likhithkrishna@Likhiths-Macbook-Pro.local>
2024-02-18 13:07:29 +0530

    Apple Watch Series 9

--- src/utils/products.js
+++ src/utils/products.js
@@ -40,6 +40,8 @@ import phone39 from "../Images/IMG_7832.JPG";
 import phone40 from "../Images/IMG_7827.JPG";
 import phone41 from "../Images/IMG_7824.JPG";
 import phone42 from "../Images/IMG_7823.JPG";
+import phone43 from "../Images/IMG_7822.JPG";
+import phone44 from "../Images/IMG_7821.JPG";
 
 const Pro15 = "99,499";
 const Plus15 = "68,000";
@@ -53,6 +55,7 @@ const S20fe = "26,000";
 const S21fe = "31,000";
 const S24Ultra = "1,14,999";
 const Ultra2 = "64,999";
+const Series9 = "29,999";
 
 export const SliderData = [
   {
@@ -186,21 +189,37 @@ export const products = [
       price: S24Ultra,
   },
   {
-        id: "40",
-        categoryName: "Apple Watch Ultra 2",
-        productName: "Apple Watch Ultra 2",
-        imgUrl: phone41,
-        category: "display",
-        price: Ultra2,
-    },
-    {
-        id: "41",
-        categoryName: "Apple Watch Ultra 2",
-        productName: "Apple Watch Ultra 2",
-        imgUrl: phone42,
-        category: "40",
-        price: Ultra2,
-    },
+      id: "40",
+      categoryName: "Apple Watch Ultra 2",
+      productName: "Apple Watch Ultra 2",
+      imgUrl: phone41,
+      category: "display",
+      price: Ultra2,
+  },
+  {
+      id: "41",
+      categoryName: "Apple Watch Ultra 2",
+      productName: "Apple Watch Ultra 2",
+      imgUrl: phone42,
+      category: "40",
+      price: Ultra2,
+  },
+  {
+      id: "42",
+      categoryName: "Apple Watch Series 9",
+      productName: "Apple Watch Series 9",
+      imgUrl: phone43,
+      category: "display",
+      price: Series9,
+  },
+  {
+      id: "43",
+      categoryName: "Apple Watch Series 9",
+      productName: "Apple Watch Series 9",
+      imgUrl: phone44,
+      category: "42",
+      price: Series9,
+  },
   {
     id: "14",
     categoryName: "14/14 Plus",
