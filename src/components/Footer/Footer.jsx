32b28fb70963c866f8d733e7d6fc94533c7fbbe5
Likhith Krishna <likhithkrishna@Likhiths-Macbook-Pro.local>
2024-10-27 12:48:04 +0530

    remove all and add 16 pro

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,19 +13,19 @@ const Footer = () => {
         <p>
           Currently online payment is not enabled due to technical issues. Will
           be updated shortly... <br /> To avail best offers please contact Ph:
-          +91 9148114488 / Visit store @
-          <a
-            href="https://g.co/kgs/kXxJP9"
-            style={{ fontWeight: "bold", textDecorationColor: "red" }}
-          >
-            📍
-          </a>
+          +91 9148114488
+{/*           <a */}
+{/*             href="https://g.co/kgs/kXxJP9" */}
+{/*             style={{ fontWeight: "bold", textDecorationColor: "red" }} */}
+{/*           > */}
+{/*             📍 */}
+{/*           </a> */}
         </p>
         <h2>Contact Us</h2>
         <ul>
-          <li>
-            Near bus stand , New bridge road , above Renukamba clinic(1st floor){" "}
-          </li>
+{/*           <li> */}
+{/*             Near bus stand , New bridge road , above Renukamba clinic(1st floor){" "} */}
+{/*           </li> */}
           <li>Email: jeevanmanjunath16@gmail.com</li>
           <li>Phone: +91 9148114488</li>
         </ul>
